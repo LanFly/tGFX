083676aa0b4ae30dfd14ad2f10c3038b4e7242d8
LanFly <1260022720@qq.com>
2020-02-19 14:43:53 +0800

    debug clang-format

--- scripts/run-clang-format.sh
+++ scripts/run-clang-format.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
-clang-format --version
+clang_format_executable="clang-format"
 
-./scripts/run-clang-format.py -r include src examples
+if command -v clang-format-9 > /dev/null; then
+  clang_format_executable=clang-format-9
+fi
+
+if command -v clang-format-10 > /dev/null; then
+  clang_format_executable=clang-format-10
+fi
+
+${clang_format_executable} --version
+
+./scripts/run-clang-format.py --clang-format-executable ${clang_format_executable} -r include src examples
